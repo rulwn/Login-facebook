614b7527270dbfcee1273dc2bc5144a5b49dc167
Alejandro <20230205@ricaldone.edu.sv>
2024-02-02 14:19:51 -0600

    Javascript Creado

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function IniciarSesión(){
+    var name = document.getElementById("").value
+    var password = document.getElementById("").value
+
+    if (password != "" && name != ""){
+        alert("Bienvenido " + name + " a nuestra página.")
+    }
+}
